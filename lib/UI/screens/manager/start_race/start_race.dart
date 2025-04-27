3f146bcdcdd1b6fb95ebf4546aeaae298e81934c
Dolla2021 <rydola57@gmail.com>
2025-04-27 14:57:07 +0700

    button start and reset

--- lib/UI/screens/manager/start_race/start_race.dart
+++ lib/UI/screens/manager/start_race/start_race.dart
@@ -10,28 +10,82 @@ class StartRace extends StatelessWidget {
     return Container(
       color: AppColors.backGroundColor,
       child: Padding(
-        padding: EdgeInsets.all(16),
-        child: Container(
-          width: double.infinity,
-          color: AppColors.backGroundColor,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Time Count',
-                style: TextStyle(
-                  fontSize: AppTextStyles.heading.fontSize,
-                  fontWeight: AppTextStyles.heading.fontWeight,
-                ),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Time Count',
+              style: TextStyle(
+                fontSize: AppTextStyles.heading.fontSize,
+                fontWeight: AppTextStyles.heading.fontWeight,
               ),
-              SizedBox(height: AppSpacings.xl),
-              TimeCount(
+            ),
+            const SizedBox(height: 32),
+            Center(
+              child: TimeCount(
                 fontSize: AppTextStyles.watch.fontSize!,
                 fontWeight: AppTextStyles.watch.fontWeight!,
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 120,
+                  height: 50,
+                  color: Colors.grey[200], // <-- No border radius = Square
+                  child: TextButton(
+                    onPressed: () {
+                      // tuk ter implementasi
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                        // <-- Make it square
+                      ),
+                    ),
+                    child: const Text(
+                      'Reset',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                Container(
+                  width: 120,
+                  height: 50,
+                  color: Colors.green, // <-- No border radius = Square
+                  child: TextButton(
+                    onPressed: () {
+                      //jam ter implementasi
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero, // <-- Make it square
+                      ),
+                    ),
+                    child: const Text(
+                      'Start',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
