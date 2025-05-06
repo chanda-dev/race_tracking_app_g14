47e45f6f81a1813b9319d2b2d3f8bb4ce89f54ee
sinraseth <sinraseth588@gmail.com>
2025-05-06 11:08:22 +0700

    RTA14-44

---
+++ lib/UI/screens/time_tracker/widget/tracking_table.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../../models/participant/participant_model.dart';
+import '../../../providers/async_value.dart';
+import '../../../providers/participant_provider.dart';
+import '../../../theme/theme.dart';
+
+class TrackingTable extends StatelessWidget {
+  const TrackingTable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final participantProvider = Provider.of<ParticipantProvider>(context);
+    Widget content = const Text('Not Work');
+    if (participantProvider.isLoading) {
+      content = const Center(child: CircularProgressIndicator());
+    } else if (participantProvider.hasData) {
+      List<Participant> fetchParticipant =
+          participantProvider.participantState!.data!;
+
+      if (fetchParticipant.isEmpty) {
+        content = const Center(child: Text("No Progress Yet"));
+      } else {
+        content = Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Participant Tracking',
+                    style: TextStyle(
+                      fontSize: AppTextStyles.heading.fontSize,
+                      fontWeight: AppTextStyles.heading.fontWeight,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  const Spacer(),
+                  IconButton(icon: const Icon(Icons.search), onPressed: () {}),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(color: AppColors.primary),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 16,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('BIB', style: TextStyle(color: AppColors.white)),
+                          Text(
+                            'NAME',
+                            style: TextStyle(color: AppColors.white),
+                          ),
+                          Text(
+                            'OVERALL TIME',
+                            style: TextStyle(color: AppColors.white),
+                          ),
+                          Text('STATUS',
+                            style: TextStyle(color: AppColors.white)),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Scrollbar(
+                        thumbVisibility: true,
+                        child: ListView.builder(
+                          itemCount: fetchParticipant.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 8.0,
+                                horizontal: 16,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(Icons.close,
+                                            color: Colors.red),
+                                        onPressed: () {
+                                          // Handle Untrack action
+                                        },
+                                      ),
+                                      IconButton(
+                                        icon: const Icon(Icons.check,
+                                            color: Colors.green),
+                                        onPressed: () {
+                                          // Handle Finished action
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      }
+    } else if (participantProvider.participantState?.state ==
+        AsyncValueState.error) {
+      content = Center(
+        child: Text(
+          'Error fetching data: ${participantProvider.participantState?.error}',
+          style: const TextStyle(color: Colors.red),
+        ),
+      );
+    }
+    return Container(child: content);
+  }
+}
